2dd73002bddbd430aa23c662a19cb5abd54b0750
Kayvan Vakili <kvakili813@gmail.com>
2018-09-11 04:03:42 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts, only: [:index, :show, :new, :create, :edit]
-  get 'posts/:id/post_data', to: 'posts#post_data'
+  get 'home', to: 'static#home'
+  resources :orders
+  resources :invoices
+  resources :products, only: [:show, :index, :new, :create]
+  get '/products/:id/description', to: 'products#description'
+  get '/products/:id/inventory', to: 'products#inventory'
+  # get '/products/:id/data', to: 'products#data'
 end
