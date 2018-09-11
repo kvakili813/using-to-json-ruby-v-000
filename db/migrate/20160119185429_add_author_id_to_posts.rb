0006a07d913dde6b0a42a828adc6a2bb7888bd7c
Kayvan Vakili <kvakili813@gmail.com>
2018-09-11 04:53:12 +0000

    automatically backed up by learn

--- db/migrate/20160119185429_add_author_id_to_posts.rb
+++ db/migrate/20160119185429_add_author_id_to_posts.rb
@@ -1,5 +1,7 @@
 class AddAuthorIdToPosts < ActiveRecord::Migration
   def change
     add_column :posts, :author_id, :integer
+    add_column :posts, :description, :string
+    add_column :posts, :inventory, :integer
   end
 end
