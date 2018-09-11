e7918b7efbc46c0c3968e17540fb3910248f2713
Kayvan Vakili <kvakili813@gmail.com>
2018-09-11 04:02:46 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,44 +1,43 @@
-class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
-
+class ProductsController < ApplicationController
   def index
-    @posts = Post.all
+    @products = Product.all
   end
 
-  def show
+  def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory > 0 ? true : false
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
   end
 
   def new
-    @post = Post.new
+    @product = Product.new
   end
 
   def create
-    @post = Post.create(post_params)
-    @post.save
-    redirect_to post_path(@post)
+    Product.create(product_params)
+    redirect_to products_path
   end
 
-  def edit
+  def show
+    @product = Product.find(params[:id])
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render json: @product.to_json(only: [:name, :description, :price, :inventory])}
+    end
   end
 
-  def update
-    @post.update(post_params)
-    redirect_to post_path(@post)
-  end
+  # def data
+  #   product = Product.find(params[:id])
+  #   render json: ProductSerializer.serialize(product)
+  # end
 
-  def post_data
-    post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
-  end
+  private
 
-private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_post
-    @post = Post.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def post_params
-    params.require(:post).permit(:title, :description)
+  def product_params
+    params.require(:product).permit(:name, :description, :inventory, :price)
   end
 end
