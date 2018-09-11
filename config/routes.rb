efe7e661f103bbe33b75412cb46fffb98ec69c90
Kayvan Vakili <kvakili813@gmail.com>
2018-09-11 04:59:26 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :orders
   resources :authors
   resources :posts, only: [:show, :index, :new, :create]
-  get '/products/:id/description', to: 'products#description'
-  get '/products/:id/inventory', to: 'products#inventory'
+  get '/posts/:id/description', to: 'posts#description'
+  get '/posts/:id/inventory', to: 'posts#inventory'
   # get '/products/:id/data', to: 'products#data'
 end
